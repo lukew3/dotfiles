f458c9623fb325c67a8506edf6b0f23a76bd37a3
Luke Weiler <lukew25073@gmail.com>
2025-03-01 14:41:30 -0500

    add install_packages.sh

---
+++ install_packages.sh
@@ -0,0 +1,17 @@
+sudo apt update
+sudo apt install -y \
+	polybar 
+
+git clone https://github.com/maestrogerardo/i3-gaps-deb.git
+cd i3-gaps-deb
+./i3-gaps-deb
+cd ..
+rm -rf i3-gaps-deb
+
+# Install alacritty and dependencies
+sudo apt install cmake g++ pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+# Install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# Install alacritty
+cargo install alacritty
+
